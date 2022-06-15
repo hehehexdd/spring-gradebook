ca1d2d47f9e2ea53f52e0be301f4c9e9a4686b10
I553290 <lyubomir.nedyalkov@sap.com>
2022-06-15 11:37:36 +0300

    Adds DirectorDTO

---
+++ Gradebook/src/main/java/com/gradebook/Gradebook/data/dto/DirectorDTO.java
@@ -0,0 +1,62 @@
+package com.gradebook.Gradebook.data.dto;
+
+public class DirectorDTO {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String school;
+
+    public DirectorDTO() {
+
+    }
+
+    public DirectorDTO(Long id, String name, String username, String school) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.school = school;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectorDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", school='" + school + '\'' +
+                '}';
+    }
+}
