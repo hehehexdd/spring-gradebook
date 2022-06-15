3eb21a0b9babdec9e250bd62c14865bdece419fa
I553290 <lyubomir.nedyalkov@sap.com>
2022-06-15 11:40:57 +0300

    Updates Director service
    * Changes name to methods
    * Adds DTO convertor

--- Gradebook/src/main/java/com/gradebook/Gradebook/data/service/DirectorServiceImpl.java
+++ Gradebook/src/main/java/com/gradebook/Gradebook/data/service/DirectorServiceImpl.java
@@ -1,11 +1,13 @@
 package com.gradebook.Gradebook.data.service;
 
+import com.gradebook.Gradebook.data.dto.DirectorDTO;
 import com.gradebook.Gradebook.data.entity.Director;
 import com.gradebook.Gradebook.data.repo.DirectorRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,27 +22,47 @@ public class DirectorServiceImpl implements DirectorService{
     }
 
     @Override
-    public Director saveDirector(Director director) {
+    public Director save(Director director) {
         return directorRepo.save(director);
     }
 
     @Override
-    public void updateDirector(Director director) {
+    public void update(Director director) {
         directorRepo.save(director);
     }
 
     @Override
-    public void deleteDirector(Long id) {
+    public void delete(Long id) {
         directorRepo.deleteById(id);
     }
 
     @Override
-    public Director getDirector(Long id) {
+    public Director getById(Long id) {
         return directorRepo.getById(id);
     }
 
     @Override
-    public List<Director> getDirectors() {
-        return directorRepo.findAll();
+    public List<DirectorDTO> getAll() {
+        List<DirectorDTO> directorDTOS = new ArrayList<>();
+
+        directorRepo.findAll().forEach(director -> {
+            directorDTOS.add(convertToDTO(director));
+        });
+
+        return directorDTOS;
+    }
+
+    @Override
+    public DirectorDTO convertToDTO(Director director) {
+        DirectorDTO directorDTO = new DirectorDTO();
+
+        if (director != null) {
+            directorDTO.setId(director.getId());
+            directorDTO.setName(String.format("%s %s", director.getFirstName(), director.getSecondName()));
+            directorDTO.setUsername(director.getUsername());
+            directorDTO.setSchool("SCHOOL");
+        }
+
+        return directorDTO;
     }
 }
