fef886a29c3062ac5df7344870044b9eb58a23ab
I553290 <lyubomir.nedyalkov@sap.com>
2022-06-21 23:32:05 +0300

    Adds findByUsername method in Director repository

--- Gradebook/src/main/java/com/gradebook/Gradebook/repo/DirectorRepo.java
+++ Gradebook/src/main/java/com/gradebook/Gradebook/repo/DirectorRepo.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DirectorRepo extends JpaRepository<Director, Long> {
+
+    Director findByUsername(String username);
 }
