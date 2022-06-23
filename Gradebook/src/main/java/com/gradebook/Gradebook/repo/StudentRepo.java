35bb9441ddbfcc6f2d0bd7ed2442b2c818214a3d
LuboN00 <lyubomir.nedyalkov@sap.com>
2022-06-23 18:53:32 +0300

    Adds new query for Student

--- Gradebook/src/main/java/com/gradebook/Gradebook/repo/StudentRepo.java
+++ Gradebook/src/main/java/com/gradebook/Gradebook/repo/StudentRepo.java
@@ -3,5 +3,8 @@ package com.gradebook.Gradebook.repo;
 import com.gradebook.Gradebook.model.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepo extends JpaRepository<Student, Long> {
+    List<Student> getAllBySchool_Id(Long schoolId);
 }
