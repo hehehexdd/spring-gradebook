3dcca29e1d95a42ad407dd5ba9bef4fb2ad2f2c4
hehehexdd <everydaystuff42069@gmail.com>
2022-07-04 13:04:27 +0300

    cors bugfix

--- Gradebook/src/main/java/com/gradebook/Gradebook/config/DevCorsConfig.java
+++ Gradebook/src/main/java/com/gradebook/Gradebook/config/DevCorsConfig.java
@@ -1,8 +1,14 @@
 package com.gradebook.Gradebook.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@Profile("development")
+@EnableWebMvc
 public class DevCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
