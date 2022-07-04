8226489085d1fe9eecf2e2fd00aaee6dbbb2f72e
hehehexdd <everydaystuff42069@gmail.com>
2022-07-04 13:39:30 +0300

    added default constructor for JwtRequest

--- Gradebook/src/main/java/com/gradebook/Gradebook/auth/dto/JwtRequest.java
+++ Gradebook/src/main/java/com/gradebook/Gradebook/auth/dto/JwtRequest.java
@@ -4,6 +4,9 @@ public class JwtRequest {
     private String username;
     private String password;
 
+    public JwtRequest() {
+    }
+
     public JwtRequest(String username, String password) {
         this.username = username;
         this.password = password;
