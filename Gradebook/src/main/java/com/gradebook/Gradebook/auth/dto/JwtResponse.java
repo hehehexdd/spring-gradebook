49904575e60f796512d86efe9e0ca01e01c7cfb7
hehehexdd <everydaystuff42069@gmail.com>
2022-07-12 20:53:56 +0300

    added role to JwtResponse

--- Gradebook/src/main/java/com/gradebook/Gradebook/auth/dto/JwtResponse.java
+++ Gradebook/src/main/java/com/gradebook/Gradebook/auth/dto/JwtResponse.java
@@ -1,14 +1,18 @@
 package com.gradebook.Gradebook.auth.dto;
 
+import com.gradebook.Gradebook.model.entity.RoleType;
+
 public class JwtResponse {
     private Long userId;
+    private RoleType role;
     private String jwtToken;
 
     public JwtResponse() {
     }
 
-    public JwtResponse(Long userId, String jwtToken) {
+    public JwtResponse(Long userId, RoleType role,  String jwtToken) {
         this.userId = userId;
+        this.role = role;
         this.jwtToken = jwtToken;
     }
 
@@ -20,11 +24,19 @@ public class JwtResponse {
         return userId;
     }
 
+    public RoleType getRole() {
+        return role;
+    }
+
     public void setUserId(Long userId) {
         this.userId = userId;
     }
 
     public void setJwtToken(String jwtToken) {
         this.jwtToken = jwtToken;
+    }
+
+    public void setRole(RoleType role) {
+        this.role = role;
     }
 }
